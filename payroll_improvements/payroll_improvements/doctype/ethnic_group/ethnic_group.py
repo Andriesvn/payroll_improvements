f294cd7ac40720e2120655132ca425d1a60ac810
andriesvn <andriesvn@avntech.net>
2022-06-21 09:12:42 +0200

    feat: Add Ethnic Group to Employee
    Also make last name mandatory

---
+++ payroll_improvements/payroll_improvements/doctype/ethnic_group/ethnic_group.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, AvN Technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class EthnicGroup(Document):
+	pass
